169034e18a75d33858132a81b81d1929e3c03ca6
Wilder Rincon <1588@holbertonschool.com>
2020-09-28 21:13:11 -0500

    Adding navbar component

--- sempli_landing/src/app/components/shared/footer/footer.component.ts
+++ sempli_landing/src/app/components/shared/footer/footer.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styles: [
-  ]
+  templateUrl: './footer.component.html'
 })
 export class FooterComponent implements OnInit {
 
